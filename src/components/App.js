fad830f019db7b7bd08ad3ed2f555ac718723b34
Naveenkumar123-hash <120244421+Naveenkumar123-hash@users.noreply.github.com>
2023-08-14 05:37:18 +0000

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
 
 import React from "react";
 import './../styles/App.css';
-
+import { useState } from "react";
+import Input from "./Input";
 const App = () => {
+  let[input,setInput]=useState('')
+  function handlingfn(value){
+        setInput(value)
+  }
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="parent">
+         <h1>Parent Component</h1>
+         {
+          (input)&&<p>{input}</p>
+         }
+         <div className="child">
+         <h2>Child component</h2>
+         <Input handlingfn={handlingfn}/>
+         </div>
     </div>
   )
 }
---
+++ src/components/Input.js
@@ -0,0 +1,20 @@
+//import { event } from "cypress/types/jquery";
+import React from "react";
+
+
+const Input=({handlingfn})=>{
+
+
+    function clicked(event){
+       handlingfn(event.target.value)
+    }
+
+    return (
+        <div>
+            <input type="text" onChange={()=>{
+                clicked(event)
+            }} placeholder="Write Something Here"></input>
+        </div>
+    )
+}
+export default Input
